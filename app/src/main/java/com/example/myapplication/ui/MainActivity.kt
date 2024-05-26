f86dfe26d7bd6340aca4dfd4eaf3dec3be4e2ccf
nisamelia <amelianisa16@gmail.com>
2024-05-26 15:09:25 +0700

    add bottom navigation

---
+++ app/src/main/java/com/example/myapplication/ui/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.myapplication.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import com.example.myapplication.R
+import com.example.myapplication.ui.home.HomeFragment
+import com.example.myapplication.ui.learn.LearnFragment
+import com.example.myapplication.ui.profile.ProfileFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navView: BottomNavigationView = findViewById(R.id.navBottom)
+        navView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.nav_edu -> {
+                    loadFragment(LearnFragment())
+                    true
+                }
+                R.id.nav_profile -> {
+                    loadFragment(ProfileFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+
+        navView.selectedItemId = R.id.nav_home
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.commit {
+            replace(R.id.main_frame, fragment)
+        }
+    }
+}
