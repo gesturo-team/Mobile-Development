aae00d0490c75d2e91853820e11f0522e1940fd1
nisamelia <amelianisa16@gmail.com>
2024-06-11 12:53:10 +0700

    history user (some bugs)

--- app/src/main/java/com/example/myapplication/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/home/HomeFragment.kt
@@ -7,12 +7,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myapplication.detection.CameraActivity
 import com.example.myapplication.databinding.FragmentHomeBinding
+import com.example.myapplication.factory.AuthViewModelFactory
+import com.example.myapplication.factory.MainViewModelFactory
+import com.example.myapplication.ui.history.HistoryAdapter
+import com.example.myapplication.ui.main.MainViewModel
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var mainViewModel: MainViewModel
+    private lateinit var historyAdapter: HistoryAdapter
 
     companion object {
         fun newInstance() = HomeFragment()
@@ -22,6 +32,8 @@ class HomeFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        mainViewModel = ViewModelProvider(requireActivity(), AuthViewModelFactory.getInstance(requireActivity()))[MainViewModel::class.java]
 
         // TODO: Use the ViewModel
     }
@@ -39,6 +51,23 @@ class HomeFragment : Fragment() {
             val cameraIntent = Intent(activity, CameraActivity::class.java)
             activity?.startActivity(cameraIntent)
         }
+
+        historyAdapter = HistoryAdapter()
+        binding.rvHistory.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = historyAdapter
+        }
+
+        mainViewModel.historyResponse.observe(viewLifecycleOwner, Observer { response ->
+            if (response?.success == true) {
+                historyAdapter.submitList(response.data?.quiz)
+            } else {
+                // Handle error
+            }
+        })
+
+        // Muat data riwayat
+        mainViewModel.getHistory()
     }
 
     override fun onDestroyView() {
